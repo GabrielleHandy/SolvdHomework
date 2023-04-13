838263c041115f583d12ca3ee5de4bf4df975b22
GabrielleHandy <91081738+GabrielleHandy@users.noreply.github.com>
2023-04-13 19:35:42 +0800

    starting on the bubble sort function

---
+++ Week_1/bubbleSort.java
@@ -0,0 +1,14 @@
+class bubbleSort {
+    public static void main (String args[]){
+        int unsorted ={3,5,1,2,4};
+        int len = unsorted.length;
+
+        for(int i = 0; i < n-1; i++){
+            for(int j=0; j< n-i-1; j++){
+                if(unsorted[j] > unsorted[j+1]){
+                    
+                }
+            }
+        }
+    }
+}
